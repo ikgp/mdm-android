224d338cf280a2286a7639a0c1d4e64c29860861
Aaron Dewes <aaron@nirvati.org>
2025-05-01 03:55:50 +0200

    fix: Make uploader also enable update directly

--- upload_new_apk_to_mdm.py
+++ upload_new_apk_to_mdm.py
@@ -1,10 +1,12 @@
-import requests # https://h-mdm.ikgp.de/rest/private/web-ui-files/raw
+import requests  # https://h-mdm.ikgp.de/rest/private/web-ui-files/raw
 import sys
 import os
 import hashlib
 
 if len(sys.argv) != 5:
-    print("Usage: python upload_new_apk_to_mdm.py <username> <password> <apk_path> <commit>")
+    print(
+        "Usage: python upload_new_apk_to_mdm.py <username> <password> <apk_path> <commit>"
+    )
     sys.exit(1)
 
 username = sys.argv[1]
@@ -18,10 +20,13 @@ if not os.path.exists(apk_path):
 
 client = requests.Session()
 
-resp = client.post("https://h-mdm.ikgp.de/rest/public/auth/login", json={
-    "login": username,
-    "password": hashlib.md5(password.encode()).hexdigest().upper(),
-})
+resp = client.post(
+    "https://h-mdm.ikgp.de/rest/public/auth/login",
+    json={
+        "login": username,
+        "password": hashlib.md5(password.encode()).hexdigest().upper(),
+    },
+)
 if resp.status_code != 200:
     print(f"Login failed: {resp.status_code} {resp.text}")
     sys.exit(1)
@@ -34,21 +39,98 @@ if "status" in data and data["status"] == "ERROR":
 # Post the file as form data to https://h-mdm.ikgp.de/rest/private/web-ui-files/raw
 with open(apk_path, "rb") as f:
     files = {
-        "file": (os.path.basename(apk_path).removesuffix(".apk")+"-" + commit+".apk", f, "application/vnd.android.package-archive"),
+        "file": (
+            os.path.basename(apk_path).removesuffix(".apk") + "-" + commit + ".apk",
+            f,
+            "application/vnd.android.package-archive",
+        ),
     }
-    resp = client.post("https://h-mdm.ikgp.de/rest/private/web-ui-files/raw", files=files)
+    resp = client.post(
+        "https://h-mdm.ikgp.de/rest/private/web-ui-files/raw", files=files
+    )
     if resp.status_code != 200:
         print(f"File upload failed: {resp.status_code} {resp.text}")
         sys.exit(1)
     path = resp.json()["data"]["serverPath"]
 
 # POST to https://h-mdm.ikgp.de/rest/private/web-ui-files/move with the path as JSON
-resp = client.post("https://h-mdm.ikgp.de/rest/private/web-ui-files/move", json={
-    "path": path,
-})
+resp = client.post(
+    "https://h-mdm.ikgp.de/rest/private/web-ui-files/move",
+    json={
+        "path": path,
+    },
+)
 
 # TODO: This gives us a 500, but works anyway
-file_path = "https://h-mdm.nirvati.org/files/" + os.path.basename(apk_path).removesuffix(".apk")+"-" + commit+".apk"
+file_path = (
+    "https://h-mdm.nirvati.org/files/"
+    + os.path.basename(apk_path).removesuffix(".apk")
+    + "-"
+    + commit
+    + ".apk"
+)
 print(f"File uploaded to{file_path}")
 
-resp = client.put("https://h-mdm.ikgp.de/rest/private/applications/versions", json={"system":False,"applicationId":18,"arch":None,"url":file_path,"version":commit,"autoUpdate":True})
+resp = client.put(
+    "https://h-mdm.ikgp.de/rest/private/applications/versions",
+    json={
+        "system": False,
+        "applicationId": 18,
+        "arch": None,
+        "url": file_path,
+        "version": commit,
+        "autoUpdate": True,
+    },
+)
+data = resp.json()
+new_id = data["data"]["id"]
+
+payload = {
+    "applicationVersionId": new_id,
+    "configurations": [
+        {
+            "id": None,
+            "customerId": 1,
+            "configurationId": 4,
+            "configurationName": "Entwicklermodus",
+            "applicationId": 18,
+            "applicationName": "MDM Agent",
+            "applicationVersionId": new_id,
+            "versionText": new_id,
+            "showIcon": True,
+            "screenOrder": None,
+            "keyCode": None,
+            "bottom": False,
+            "longTap": False,
+            "action": "1",
+            "remove": False,
+            "notify": True,
+            "common": False,
+            "selected": True,
+        },
+        {
+            "id": None,
+            "customerId": 1,
+            "configurationId": 1,
+            "configurationName": "Standard-Tafel",
+            "applicationId": 18,
+            "applicationName": "MDM Agent",
+            "applicationVersionId": new_id,
+            "versionText": new_id,
+            "showIcon": False,
+            "screenOrder": None,
+            "keyCode": None,
+            "bottom": False,
+            "longTap": False,
+            "action": "1",
+            "remove": False,
+            "notify": True,
+            "common": False,
+            "selected": True,
+        },
+    ],
+}
+
+client.post("https://h-mdm.ikgp.de/rest/private/applications/version/configurations", json=payload)
+
+print(resp.text)
